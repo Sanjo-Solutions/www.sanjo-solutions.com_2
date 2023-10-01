37804c20bb1edaf10be2f1877e344e9df9b70460
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-10-01 13:28:09 +0200

    Work

--- sanjo-solutions/functions.php
+++ sanjo-solutions/functions.php
@@ -1,6 +1,11 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'sanjo_solutions_enqueue_styles' );
-function sanjo_solutions_enqueue_styles() {
+
+function sanjo_solutions_wp_enqueue_styles() {
+    wp_enqueue_style( 'sanjo-solutions-bootstrap',
+		get_stylesheet_directory_uri() . '/bootstrap.min.css',
+		array(),
+		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
+	);
 	wp_enqueue_style( 'sanjo-solutions-style',
 		get_stylesheet_uri(),
 		array(),
@@ -8,17 +13,13 @@ function sanjo_solutions_enqueue_styles() {
 	);
 }
 
-add_action('wp_head', 'sanjo_solutions_wp_head');
-function sanjo_solutions_wp_head() {
-	global $post;
-  if ($post->ID == 31) {
-		echo <<<EOT
-<link
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-		rel="stylesheet"
-		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-		crossorigin="anonymous"
->
-EOT;
-  }
+add_action( 'wp_enqueue_scripts', 'sanjo_solutions_wp_enqueue_styles' );
+
+function sanjo_solutions_after_setup_theme() {
+	add_editor_style(array(
+		get_stylesheet_directory_uri() . '/bootstrap.min.css',
+		get_stylesheet_uri()
+	));
 }
+
+add_action( 'after_setup_theme', 'sanjo_solutions_after_setup_theme' );
--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 scp -r -i "~/.ssh/sanjo-solutions.pem" sanjo-solutions ubuntu@ec2-3-77-171-31.eu-central-1.compute.amazonaws.com:/var/www/html/wp-content/themes
 # rsync -a --delete -e "ssh -i ~/.ssh/sanjo-solutions.pem" sanjo-solutions/ ubuntu@ec2-3-77-171-31.eu-central-1.compute.amazonaws.com:/var/www/html/wp-content/themes/sanjo-solutions/
 
-scp -r -i "~/.ssh/sanjo-solutions.pem" translatepress-developer ubuntu@ec2-3-77-171-31.eu-central-1.compute.amazonaws.com:/var/www/html/wp-content/plugins
+# scp -r -i "~/.ssh/sanjo-solutions.pem" translatepress-developer ubuntu@ec2-3-77-171-31.eu-central-1.compute.amazonaws.com:/var/www/html/wp-content/plugins
